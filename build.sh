9cd902295ce6b4904b696d546162934d4e2ff68b
Joey Dumont <joey.dumont@gmail.com>
2015-02-26 19:16:08 -0500

    Added build script.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# --------------------------------------------------------------------------- #
+# Author:       Joey Dumont                   <joey.dumont@gmail.com>         #
+# Author:       Denis Gagnon                  <gagnon88@gmail.com>            #
+# Date:         2015-02-26                                                    #
+# Description:  Compiles and builds complex_bessel                            #
+# ----------------------------------------------------------------------------#
+
+# -----------------------------------------#
+# This script simply calls CMake from the  #
+# build directory and compiles the library.#
+#                                          #
+# Usage: bash build.sh {make_arg}          #
+# -----------------------------------------#
+
+# Change to file directory. 
+cd "$(dirname "$(realpath "$0")")";
+
+# Check if build/ dir exists. 
+if [ ! -d build ]; then
+    mkdir build
+fi 
+
+# Change to build dir and compile the library.
+cd build
+cmake ..
+make $1
